9deed54b21516526a41ac2da08557cdd45486af0
jakemcquade <183949683+jakemcquade@users.noreply.github.com>
2026-02-01 10:08:52 -0500

    Updated Constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,29 +4,50 @@
 
 package frc.robot;
 
+import static edu.wpi.first.units.Units.Degrees;
+import static edu.wpi.first.units.Units.Inches;
+import static edu.wpi.first.units.Units.Meters;
+import static edu.wpi.first.units.Units.Radians;
+
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.DriverStation;
+import swervelib.math.Matter;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean constants. This
- * class should not be used for any other purpose. All constants should be declared globally (i.e. public static). Do
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean constants. This class should not be used for any other
+ * purpose. All constants should be declared globally (i.e. public static). Do
  * not put anything functional in this class.
- *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the constants are needed, to reduce verbosity.
  */
 public final class Constants {
-  public static final double MAX_SPEED  = Units.feetToMeters(14.5); // Maximum speed of the robot in meters per second, used to limit acceleration.
-
-  public static final class DrivebaseConstants {
+    public static final double ROBOT_MASS = Units.lbsToKilograms(50);
+    public static final Matter CHASSIS = new Matter(new Translation3d(0, 0, Units.inchesToMeters(8)), ROBOT_MASS);
+    public static final double MAX_SPEED = Units.feetToMeters(14.5); // Robot maximum speed (m/s). Used to limit acceleration.
     public static final double WHEEL_LOCK_TIME = 10; // Hold time on motor brakes when disabled (seconds)
-  }
+    public static final double LOOP_TIME = 0.13; // s, 20ms + 110ms spark max velocity lag
 
-  public static class OperatorConstants {
-    // Joystick Deadband
-    public static final double RIGHT_X_DEADBAND = 0.1;
-    public static final double LEFT_Y_DEADBAND = 0.1;
-    public static final double TURN_CONSTANT = 6;
-    public static final double DEADBAND = 0.1;
-    public static final int PORT = 0;
-  }
+    public static class Limelight {
+        public static final Boolean enabled = false;
+        public static final Pose3d offset = new Pose3d(
+                Inches.of(0).in(Meters),
+                Inches.of(0).in(Meters),
+                Inches.of(0).in(Meters),
+                new Rotation3d(0, Degrees.of(0).in(Radians), Degrees.of(0).in(Radians)));
+    }
+
+    public static class OperatorConstants {
+        public static final double SCALE_TRANSLATION = 0.8;
+        public static final int kDriverControllerPort = 0;
+        public static final double DEADBAND = 0.1; // Joystick Deadband
+    }
+
+    public static class IndexerConstants {
+        public static final int kIndexMotorId = 0;
+    }
 }
