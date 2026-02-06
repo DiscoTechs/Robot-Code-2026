c23f361c16a2ea46c2f33539f9c4a161e034e50b
DiscoTechs <36282374+DiscoTechs@users.noreply@github.com>
2026-02-05 20:41:58 -0500

    Better DriveToTarget Code

--- src/main/java/frc/robot/commands/DriveToTarget.java
+++ src/main/java/frc/robot/commands/DriveToTarget.java
@@ -1,16 +1,22 @@
 package frc.robot.commands;
 
+import java.util.Optional;
+
 import org.littletonrobotics.junction.Logger;
 
-import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.SwerveSubsystem;
-import limelight.networktables.LimelightResults;
-import limelight.networktables.target.AprilTagFiducial;
+import limelight.networktables.LimelightPoseEstimator.EstimationMode;
+import limelight.results.RawFiducial;
+import limelight.networktables.PoseEstimate;
 
 // TODO: Verify that this code works
 public class DriveToTarget extends Command {
+    private final PIDController turnPID = new PIDController(0.03, 0, 0.001);
     private final SwerveSubsystem drivetrain;
+    private double lastDistance = 10;
 
     public DriveToTarget(SwerveSubsystem dt) {
         this.drivetrain = dt;
@@ -24,16 +30,28 @@ public class DriveToTarget extends Command {
 
     @Override
     public void execute() {
-        LimelightResults result = drivetrain.getLimelight().getLatestResults().get();
-        AprilTagFiducial tag = result.targets_Fiducials[0];
-        Pose2d targetPose = tag.getTargetPose_RobotSpace2D();
+        Optional<PoseEstimate> visionEstimate = drivetrain.getLimelight().createPoseEstimator(EstimationMode.MEGATAG2).getPoseEstimate();
+        visionEstimate.ifPresent((PoseEstimate poseEstimate) -> {
+            if (poseEstimate.tagCount > 0) {
+                RawFiducial tag = poseEstimate.rawFiducials[0];
+                lastDistance = tag.distToCamera;
 
-        Logger.recordOutput("Limelight/targetPose", targetPose);
-        drivetrain.driveToPose(targetPose);
+                double rotation = turnPID.calculate(tag.txnc, 0);
+                if (Math.abs(tag.txnc) < 1.0) {
+                    rotation = 0;
+                }
+
+                drivetrain.getSwerveDrive().drive(new Translation2d(0.75, 0), rotation, false, false);
+                Logger.recordOutput("Limelight/targetDistance", lastDistance);
+                Logger.recordOutput("Limelight/targetX", tag.txnc);
+            } else {
+                Logger.recordOutput("Limelight/targetDistance", 0);
+            }
+        });
     }
 
     @Override
     public boolean isFinished() {
-        return false;
+        return lastDistance < 1.15;
     }
 }
