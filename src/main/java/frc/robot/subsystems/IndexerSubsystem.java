72c2a7f7559ef4b655bbf8c4724d17a21ed2948a
jakemcquade <183949683+jakemcquade@users.noreply.github.com>
2026-02-09 12:24:50 -0500

    Fixed Formatting

--- src/main/java/frc/robot/subsystems/IndexerSubsystem.java
+++ src/main/java/frc/robot/subsystems/IndexerSubsystem.java
@@ -6,25 +6,22 @@ import static edu.wpi.first.units.Units.Pounds;
 import static edu.wpi.first.units.Units.RPM;
 import static edu.wpi.first.units.Units.RotationsPerSecondPerSecond;
 
-import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.ctre.phoenix6.hardware.TalonFX;
-import com.revrobotics.spark.SparkMax;
-
-import edu.wpi.first.math.system.plant.DCMotor;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
-import yams.gearing.GearBox;
-import yams.gearing.MechanismGearing;
-import yams.mechanisms.config.FlyWheelConfig;
-import yams.mechanisms.velocity.FlyWheel;
-import yams.motorcontrollers.SmartMotorController;
-import yams.motorcontrollers.SmartMotorControllerConfig;
-import yams.motorcontrollers.SmartMotorControllerConfig.ControlMode;
-import yams.motorcontrollers.SmartMotorControllerConfig.MotorMode;
 import yams.motorcontrollers.SmartMotorControllerConfig.TelemetryVerbosity;
-import yams.motorcontrollers.local.SparkWrapper;
+import yams.motorcontrollers.SmartMotorControllerConfig.MotorMode;
+import yams.motorcontrollers.SmartMotorControllerConfig.ControlMode;
+import yams.motorcontrollers.SmartMotorControllerConfig;
+import yams.motorcontrollers.SmartMotorController;
 import yams.motorcontrollers.remote.TalonFXWrapper;
+import yams.mechanisms.velocity.FlyWheel;
+import yams.mechanisms.config.FlyWheelConfig;
+import yams.gearing.MechanismGearing;
+import yams.gearing.GearBox;
+import frc.robot.Constants;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.math.system.plant.DCMotor;
+import com.ctre.phoenix6.hardware.TalonFX;
 
 public class IndexerSubsystem extends SubsystemBase {
     private TalonFX talonFX = new TalonFX(Constants.IndexerConstants.INDEXER_MOTOR_CAN_ID);
@@ -62,7 +59,5 @@ public class IndexerSubsystem extends SubsystemBase {
     public void periodic() { indexer.updateTelemetry(); }
 
     @Override
-    public void simulationPeriodic() {
-        indexer.simIterate();
-    }
+    public void simulationPeriodic() { indexer.simIterate(); }
 }
