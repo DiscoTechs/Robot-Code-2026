2418b0c50b81600c2fb63ef16fdf23e5b655f04f
jakemcquade <183949683+jakemcquade@users.noreply.github.com>
2026-02-18 09:22:35 -0500

    TODO: Test Indexer Code

--- src/main/java/frc/robot/subsystems/IndexerSubsystem.java
+++ src/main/java/frc/robot/subsystems/IndexerSubsystem.java
@@ -1,25 +1,87 @@
 package frc.robot.subsystems;
 
+import static edu.wpi.first.units.Units.Amps;
+import static edu.wpi.first.units.Units.Inches;
+import static edu.wpi.first.units.Units.Pounds;
+import static edu.wpi.first.units.Units.RPM;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix6.hardware.TalonFX;
+import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.IndexerConstants;
+import edu.wpi.first.wpilibj.DigitalInput;
+
+import yams.gearing.GearBox;
+import yams.gearing.MechanismGearing;
+import yams.mechanisms.config.FlyWheelConfig;
+import yams.mechanisms.velocity.FlyWheel;
+import yams.motorcontrollers.SmartMotorController;
+import yams.motorcontrollers.SmartMotorControllerConfig;
+import yams.motorcontrollers.SmartMotorControllerConfig.ControlMode;
+import yams.motorcontrollers.SmartMotorControllerConfig.MotorMode;
+import yams.motorcontrollers.SmartMotorControllerConfig.TelemetryVerbosity;
+import yams.motorcontrollers.remote.TalonFXWrapper;
 
 public class IndexerSubsystem extends SubsystemBase {
-        public IndexerSubsystem() {}
-    
-        public Command forward() {
-            return Commands.runOnce(() -> {});
-        }
+    private SmartMotorController smctl;
+    private FlyWheel indexer;
+    private TalonFX motor;
 
-        public Command reverse() {
-            return Commands.runOnce(() -> {});
-        }
+    private static final double DEFAULT_SPEED = 0.85;
 
-        public Command setSpeed(double speed) {
-            return Commands.runOnce(() -> {});
-        }
+    public IndexerSubsystem() {
+        SmartMotorControllerConfig config = new SmartMotorControllerConfig()
+            .withGearing(new MechanismGearing(GearBox.fromReductionStages(1)))
+            .withStatorCurrentLimit(Amps.of(20))
+            .withMotorInverted(false)
+            .withControlMode(ControlMode.OPEN_LOOP)
+            .withIdleMode(MotorMode.BRAKE)
+            .withTelemetry("IndexerMotor", TelemetryVerbosity.HIGH);
 
-        public Command stop() {
-            return Commands.runOnce(() -> {});
+        this.motor = new TalonFX(IndexerConstants.INDEXER_MOTOR_CAN_ID);
+        this.smctl = new TalonFXWrapper(motor, DCMotor.getKrakenX60(1), config);
+        this.indexer = new FlyWheel(new FlyWheelConfig(smctl)
+            .withDiameter(Inches.of(1.5))
+            .withMass(Pounds.of(0.2))
+            .withSoftLimit(RPM.of(-6000), RPM.of(6000))
+            .withTelemetry("IndexerWheel", TelemetryVerbosity.HIGH));
+    }
+
+    public Command forward() {
+        return indexer.set(DEFAULT_SPEED);
+    }
+
+    public Command reverse() {
+        return indexer.set(-DEFAULT_SPEED);
+    }
+
+    public Command setSpeed(double speed) {
+        return indexer.set(speed);
+    }
+
+    public Command stop() {
+        return indexer.set(0.0);
+    }
+
+    public Command forwardUntilSensor(DigitalInput beamBreak) {
+        return Commands.run(() -> indexer.set(DEFAULT_SPEED), this)
+                .until(() -> !beamBreak.get())
+                .andThen(stop());
+    }
+
+    @Override
+    public void periodic() {
+        if (indexer != null) {
+            indexer.updateTelemetry();
         }
+    }
+
+    @Override
+    public void simulationPeriodic() {
+        if (indexer != null) {
+            indexer.simIterate();
+        }
+    }
 }
