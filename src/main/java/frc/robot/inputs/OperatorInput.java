69cec65fb533ee066d88b050de55181511818072
DiscoTechs <36282374+DiscoTechs@users.noreply@github.com>
2026-03-07 11:13:22 -0500

    BETTER INPUT

--- src/main/java/frc/robot/inputs/OperatorInput.java
+++ src/main/java/frc/robot/inputs/OperatorInput.java
@@ -4,6 +4,9 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import static edu.wpi.first.units.Units.Degrees;
+
+import org.littletonrobotics.junction.Logger;
+
 import frc.robot.subsystems.OperatorSubsystem;
 
 public class OperatorInput {
@@ -30,7 +33,7 @@ public class OperatorInput {
             controller.povDown()
                     .whileTrue(operator.climber.climbDown())
                     .onFalse(operator.climber.stop());
-//climber up and down are inverted 
+            // climber up and down are inverted
             controller.povUp()
                     .whileTrue(operator.climber.climbUp())
                     .onFalse(operator.climber.stop());
@@ -47,27 +50,61 @@ public class OperatorInput {
             }, operator.turret));
         }
 
-        controller.rightTrigger().onTrue(operator.intakeAll());
-        controller.rightTrigger().onFalse(operator.stopAll());
+        operator.shooter.setDefaultCommand(new RunCommand(() -> {
+            double trig = controller.getRightTriggerAxis();
 
-        // controller.leftTrigger().onTrue(Commands.runOnce(() -> operator.shooter.reverse()));
-        // controller.leftTrigger().onFalse(Commands.runOnce(() -> operator.shooter.stop()));
+            if (trig < 0.2) {
+                Logger.recordOutput("ShooterSpeed", 0);
+                operator.shooter.set(0).execute();
+            } else {
+                if (0.2 <= trig && trig <= 0.5) {
+                    Logger.recordOutput("ShooterSpeed", 0.35);
+                    operator.shooter.set(0.35).execute();
+                } else if (0.5 <= trig && trig <= 0.75) {
+                    Logger.recordOutput("ShooterSpeed", 0.6);
+                    operator.shooter.set(0.6).execute();
+                } else if (0.76 <= trig && trig <= 1) {
+                    Logger.recordOutput("ShooterSpeed", 0.75);
+                    operator.shooter.set(0.75).execute();
+                }
+            }
+        }, operator.shooter));
+        // controller.leftTrigger().onTrue(Commands.runOnce(() ->
+        // operator.shooter.reverse()));
+        // controller.leftTrigger().onFalse(Commands.runOnce(() ->
+        // operator.shooter.stop()));
+
+        // controller.rightTrigger().onFalse(operator.stopAll());
+
+        controller.rightBumper().onTrue(operator.kicker.forward());
+        controller.leftBumper().onTrue(operator.kicker.stop());
 
         if (operator.intakepivot != null) {
             controller.y()
-                    .whileTrue(operator.intakepivot.setAngle(Degrees.of(45)))
+                    .whileTrue(operator.intakepivot.forward())
                     .onFalse(operator.intakepivot.stop());
-                   
 
             controller.x()
-                    .whileTrue(operator.intakepivot.setAngle(Degrees.of(-45)))
+                    .whileTrue(operator.intakepivot.back())
                     .onFalse(operator.intakepivot.stop());
         }
 
-        if (operator.intake != null) {
+        if (operator.indexer != null && operator.intake != null) {
             controller.a()
-                    .whileTrue(operator.intake.forward())
-                    .onFalse(operator.intake.stop());
+                    .whileTrue(Commands.parallel(
+                            operator.indexer.forward().asProxy(),
+                            operator.intake.forward().asProxy()))
+                    .onFalse(Commands.parallel(
+                            operator.indexer.stop().asProxy(),
+                            operator.intake.stop().asProxy()));
+
+            controller.b()
+                    .whileTrue(Commands.parallel(
+                            operator.indexer.reverse().asProxy(),
+                            operator.intake.reverse().asProxy()))
+                    .onFalse(Commands.parallel(
+                            operator.indexer.stop().asProxy(),
+                            operator.intake.stop().asProxy()));
         }
     }
 }
